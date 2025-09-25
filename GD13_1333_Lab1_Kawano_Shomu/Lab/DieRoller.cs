e293d9cf13f1962f0cacc6f6e98abf9883ed4a39
Shomu10216282 <chuanyexiangmeng@gmail.com>
2025-09-24 18:07:01 -0700

    Lab4

--- GD13_1333_Lab1_Kawano_Shomu/Lab/DieRoller.cs
+++ GD13_1333_Lab1_Kawano_Shomu/Lab/DieRoller.cs
@@ -2,21 +2,29 @@ using System;
 
 class DieRoller
 {
+    private int sides;
     private Random rand = new Random();
+
+    public DieRoller(int sides)
+    {
+        this.sides = sides;
+    }
+
 
     public int Roll()
     {
-        int d6 = rand.Next(1, 7);   
-        int d8 = rand.Next(1, 9);   
-        int d12 = rand.Next(1, 13); 
-        int d20 = rand.Next(1, 21); 
+        return rand.Next(1, sides + 1);
 
-        Console.WriteLine("Rolling dice...");
-        Console.WriteLine($"d6:  {d6}");
-        Console.WriteLine($"d8:  {d8}");
-        Console.WriteLine($"d12: {d12}");
-        Console.WriteLine($"d20: {d20}");
-
-        return d6 + d8 + d12 + d20;
+        //Lab3
+        //int d6 = rand.Next(1, 7);   
+        //int d8 = rand.Next(1, 9);   
+        //int d12 = rand.Next(1, 13); 
+        //int d20 = rand.Next(1, 21); 
+        //Console.WriteLine("Rolling dice...");
+        //Console.WriteLine($"d6:  {d6}");
+        //Console.WriteLine($"d8:  {d8}");
+        //Console.WriteLine($"d12: {d12}");
+        //Console.WriteLine($"d20: {d20}");
+        //return d6 + d8 + d12 + d20;
     }
 }

fa99c893484701936b6df8e45511b45b00b4134d
Shomu10216282 <chuanyexiangmeng@gmail.com>
2025-10-01 18:27:28 -0700

    Assignment1

---
+++ GD13_1333_Lab_Shomu/Scripts/DieRoller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GD13_1333_Lab_Shomu
+{
+    class DieRoller
+    {
+        private int sides;
+        private Random rand = new Random();
+
+        public DieRoller(int sides)
+        {
+            this.sides = sides;
+        }
+
+        public int Roll()
+        {
+            return rand.Next(1, sides + 1);
+        }
+    }
+}
---
+++ GD13_1333_Lab_Shomu/Scripts/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD13_1333_Lab_Shomu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager gm = new GameManager();
+            gm.Play();
+        }
+    }
+}

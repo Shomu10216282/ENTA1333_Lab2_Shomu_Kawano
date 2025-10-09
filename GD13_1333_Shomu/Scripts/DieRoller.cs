9fbcffd9783d110f026fd2093de1091b8d8d7479
Shomu10216282 <chuanyexiangmeng@gmail.com>
2025-10-09 06:22:52 -0700

    Lab6

---
+++ GD13_1333_Shomu/Scripts/DieRoller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD13_1333_Shomu.Scripts
+{
+    internal class DieRoller
+    {
+        private int sides;
+        private Random rand = new Random();
+
+        public DieRoller()
+        {
+        }
+
+        public DieRoller(int sides)
+        {
+            this.sides = sides;
+        }
+
+        public int Roll()
+        {
+            return rand.Next(1, sides + 1);
+        }
+    }
+}
